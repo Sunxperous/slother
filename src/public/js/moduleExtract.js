17acff8efff4b8b0e849d2806a264bf5109711be
MunAw <jasontanma@gmail.com>
2014-06-05 17:46:31 +0800

    Allow multiple lesson of similar type. Modularise some part of code

--- src/public/js/moduleExtract.js
+++ src/public/js/moduleExtract.js
@@ -1,80 +1,113 @@
 //  extract module info from nusmods address.
-//  Input: addr - address of nusmods 
-//  Output: Object of modules info.
-//  Output format eg.: <Object Name>.<Module Code>.<Info>
-//  Info includes: ModuleCode, ModuleTitle, Timetable
+//  Input        : address of nusmods 
+//  Output       : Object of modules info.
+//  Output format: <Object Name>.<Module Code>.<Info>
+//  Info includes: ModuleCode, ModuleTitle, 
+//                 Lecture array, Tutorial array, 
+//                 Sectional array and Lab array
+//  Example link: http://nusmods.com/2013-2014/sem2/
+//                v1/#IS1103=63&ST2334=2SL1&ST2334=
+//                8T9&ST2334=2SL1
+//
 //  Take note of delays from getJSON !!!
+
+
 function extract(addr) {
-  var str = addr;
-  str = str.substring(38);
-  var mod = str.split("&"); 
-  mod.sort(); 
-  console.log(mod);
-  var module = new Object();
-  var tempMod = mod.pop();
-  tempMod = tempMod.split("=");
+  var year = addr.substring(19,28),
+      sem = addr.substring(32,33),
+      modDetailInfo = {};
+  addr = addr.substring(38);
+  mod = addr.split("&");
+  mod.sort();
+  console.log(mod); //For checking
+  var tempMod = mod.pop().split("=");
   while(true) {
     
-    var modName = tempMod[0];
-    var tempOb = {};
-    while(tempMod[0] == modName) {
-        tempOb.codeNo = modName;
-        if(tempMod[1].substring(0,1)=='1') {
-          tempOb.lab = tempMod[1].substring(1);
-        }
-        else if(tempMod[1].substring(0,1)=='2') {
-          tempOb.lect = tempMod[1].substring(1);
-        }
-        else if(tempMod[1].substring(0,1)=='6') {
-          tempOb.sect = tempMod[1].substring(1);
-        }
-        else if(tempMod[1].substring(0,1)=='8') {
-          tempOb.tut = tempMod[1].substring(1);
-        }
+    var tempOb = {
+      codeNo: tempMod[0],
+      lect: [], lab: [], sect: [], tut: []
+    };
+    console.log(tempOb);
+    while(tempMod[0] == tempOb.codeNo) {
+        tempOb[noToLessonType(tempMod[1])].
+          push(tempMod[1].substring(1));  
         tempMod = mod.pop();
-        if(tempMod== undefined)
-          break;
-        tempMod = tempMod.split("=");
+        if(tempMod== undefined) { break; }
+        else 
+          tempMod = tempMod.split("="); 
     }
-    module[modName] = tempOb;
-    if(tempMod== undefined)
-      break;
-    module[modName] = tempOb;
+    modDetailInfo[tempOb.codeNo] = tempOb;
+    if(tempMod== undefined) { break; }
   }
+  console.log(modDetailInfo);
 
-  var tempURL = "http://api.nusmods.com/2013-2014/2/modules/FE5218.json",
-      tempCode = "FE5218";
-  for(var x in module) {
+
+  var tempURL = "http://api.nusmods.com/"+year+"/"
+                +sem+'/modules/FE5218.json',
+      tempModCode = "FE5218",
+      modBriefInfo = {};
+  for(var x in modDetailInfo) {
     var y = 0;
-    tempURL = tempURL.replace(tempCode,x);
-    tempCode = x;
-    var obj = [];
-    var objt = $.getJSON(tempURL,function(data) {
-      obj[data.ModuleCode] = {};
+    tempURL = tempURL.replace(tempModCode,x); 
+    tempModCode = x;
+    $.getJSON(tempURL,function(data) {
+      modBriefInfo[data.ModuleCode] = {
+        Lecture: [], Tutorial: [], 
+        Sectional: [], Laboratory: []
+      };
+      
       //Go through and copy each element.
       //First: ExamDate.
-      obj[data.ModuleCode].ExamDate = data.ExamDate;
+      modBriefInfo[data.ModuleCode].ExamDate =
+        data.ExamDate;
       //Next,Module Title and code.
-      obj[data.ModuleCode].ModuleTitle = data.ModuleTitle;
-      obj[data.ModuleCode].ModuleCode = data.ModuleCode;
+      modBriefInfo[data.ModuleCode].ModuleTitle = 
+        data.ModuleTitle;
+      modBriefInfo[data.ModuleCode].ModuleCode = 
+        data.ModuleCode;
 
       //Next,timetable.
       for(y in data.Timetable) {
         if(data.Timetable[y].LessonType == "LECTURE" && 
-            data.Timetable[y].ClassNo == module[data.ModuleCode].lect )
-          obj[data.ModuleCode].Lecture = data.Timetable[y];
+            data.Timetable[y].ClassNo == modDetailInfo[data.ModuleCode].lect )
+          modBriefInfo[data.ModuleCode].Lecture.push(data.Timetable[y]);
         else if(data.Timetable[y].LessonType == "LABORATORY" && 
-            data.Timetable[y].ClassNo == module[data.ModuleCode].lab )
-          obj[data.ModuleCode].Laboratory = data.Timetable[y];
+            data.Timetable[y].ClassNo == modDetailInfo[data.ModuleCode].lab )
+          modBriefInfo[data.ModuleCode].Laboratory.push(data.Timetable[y]);
         else if(data.Timetable[y].LessonType == "SECTIONAL TEACHING" && 
-            data.Timetable[y].ClassNo == module[data.ModuleCode].sect )
-          obj[data.ModuleCode].Sectional = data.Timetable[y];
+            data.Timetable[y].ClassNo == modDetailInfo[data.ModuleCode].sect )
+          modBriefInfo[data.ModuleCode].Sectional.push(data.Timetable[y]);
         else if(data.Timetable[y].LessonType == "TUTORIAL" && 
-            data.Timetable[y].ClassNo == module[data.ModuleCode].tut )
-          obj[data.ModuleCode].Tutorial = data.Timetable[y];
+            data.Timetable[y].ClassNo == modDetailInfo[data.ModuleCode].tut )
+          modBriefInfo[data.ModuleCode].Tutorial.push(data.Timetable[y]);
       }
-      //console.log(obj);
     });
   }
-  return obj;
+  return modBriefInfo;
 }
+
+//Return class type string
+//Input: class no. from nusMods eg. MA2213= 8T04
+function noToLessonType(lesson) {
+  switch(lesson.substring(0,1)) {
+  case '1': return "lab";
+  case '2': return "lect";
+  case '6': return "sect";
+  case '8': return "tut";
+  }
+}
+
+
+// //Return boolean whether lesson is in user timetable
+// function checkLessonNo(timetable, lessonNo) {
+//   for(var x in lessonNo) {
+//     switch(timetable.LessonType) {
+//       case "LECTURE": 
+//       case "LABORATORY": 
+//       case "TUTORIAL":
+//       case "SECTIONAL TEACHING": 
+//         return (timetable.ClassNo == lessonNo);
+//       default: return false;
+//     }
+//   }
+// }
