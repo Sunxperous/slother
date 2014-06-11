051a2c8bf5c44d0f0bc486265fcaf361ef38a7e0
Sun Wang Jun <sunxperous@gmail.com>
2014-06-11 07:43:30 +0000

    add mongodb, passportjs (provision)

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -11,6 +11,25 @@ class apt_get_update {
 }
 class { 'apt_get_update': 
   stage => preinstall,
+  require => [
+    Class['before_mongodb']
+  ]
+}
+
+# Checks before installing MongoDB.
+class before_mongodb {
+  exec { 'import_publickey':
+    command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
+    path => '/usr/bin:/bin',
+    onlyif => "dpkg-query -W -f='${Status}' mongodb-10gen 2>/dev/null | grep -c 'ok installed'"
+  }
+  file { '/etc/apt/sources.list.d/mongodb.list':
+    ensure => present,
+    content => 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen'
+  }
+}
+class { 'before_mongodb': 
+  stage => preinstall,
 }
 
 # Installs node.js via Puppet module nodejs.
@@ -53,5 +72,10 @@ package { 'git':
 
 # Installs Vim.
 package { 'vim':
+  ensure => installed,
+}
+
+# Installs MongoDB.
+package { 'mongodb-10gen':
   ensure => installed,
 }
