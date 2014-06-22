1c440c78577660fabee9e35850feb591035d2a93
Sun Wang Jun <sunxperous@gmail.com>
2014-06-22 12:24:00 +0000

    improve date display

--- src/public/js/timetable.js
+++ src/public/js/timetable.js
@@ -1,131 +1,140 @@
-'use strict';
+(function() {
 
-var mock = JSON.parse('[{"summary":"SSA1201 (LECT)","description":"Singapore Society - ClassNo: 1","location":"LT11","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-24T08:00:00.000Z"],"dateStart":"2014-01-13T08:00:00.000Z","dateEnd":"2014-01-13T10:00:00.000Z"},{"summary":"SSA1201 (TUT)","description":"Singapore Society - ClassNo: E1","location":"AS1-0204","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-12T08:00:00.000Z"],"dateStart":"2014-01-29T08:00:00.000Z","dateEnd":"2014-01-29T10:00:00.000Z"},{"summary":"SSA1201 (EXAM)","description":"Singapore Society","rrule":{"freq":"ONCE"},"dateStart":"2014-04-30T01:00:00.000Z","dateEnd":"2014-04-30T04:00:00.000Z"},{"summary":"HY2242 (LECT)","description":"Singapore\'s Military History - ClassNo: 1","location":"AS4-0206","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-24T02:00:00.000Z"],"dateStart":"2014-01-13T02:00:00.000Z","dateEnd":"2014-01-13T04:00:00.000Z"},{"summary":"HY2242 (LECT)","description":"Singapore\'s Military History - ClassNo: 1","location":"AS4-0206","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-27T04:00:00.000Z"],"dateStart":"2014-01-16T04:00:00.000Z","dateEnd":"2014-01-16T06:00:00.000Z"},{"summary":"HY2242 (TUT)","description":"Singapore\'s Military History - ClassNo: W1","location":"AS1-0204","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-10T02:00:00.000Z"],"dateStart":"2014-01-27T02:00:00.000Z","dateEnd":"2014-01-27T04:00:00.000Z"},{"summary":"HY2242 (EXAM)","description":"Singapore\'s Military History","rrule":{"freq":"ONCE"},"dateStart":"2014-05-07T01:00:00.000Z","dateEnd":"2014-05-07T04:00:00.000Z"},{"summary":"GEK1008 (LECT)","description":"Southeast Asia: A Changing Region - ClassNo: 1","location":"UT-AUD1","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-28T06:00:00.000Z"],"dateStart":"2014-01-17T06:00:00.000Z","dateEnd":"2014-01-17T08:00:00.000Z"},{"summary":"GEK1008 (TUT)","description":"Southeast Asia: A Changing Region - ClassNo: D18","location":"ERC-SR11","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-14T08:00:00.000Z"],"dateStart":"2014-01-31T08:00:00.000Z","dateEnd":"2014-01-31T10:00:00.000Z"},{"summary":"GEK1008 (EXAM)","description":"Southeast Asia: A Changing Region","rrule":{"freq":"ONCE"},"dateStart":"2014-04-28T05:00:00.000Z","dateEnd":"2014-04-28T08:00:00.000Z"},{"summary":"CS2100 (LAB)","description":"Computer Organisation - ClassNo: 14","location":"COM1-0114","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-13T08:00:00.000Z"],"dateStart":"2014-01-30T08:00:00.000Z","dateEnd":"2014-01-30T09:00:00.000Z"},{"summary":"CS2100 (LECT)","description":"Computer Organisation - ClassNo: 1","location":"i3-Aud","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-26T04:00:00.000Z"],"dateStart":"2014-01-15T04:00:00.000Z","dateEnd":"2014-01-15T06:00:00.000Z"},{"summary":"CS2100 (LECT)","description":"Computer Organisation - ClassNo: 1","location":"i3-Aud","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-27T06:00:00.000Z"],"dateStart":"2014-01-16T06:00:00.000Z","dateEnd":"2014-01-16T07:00:00.000Z"},{"summary":"CS2100 (TUT)","description":"Computer Organisation - ClassNo: 13","location":"COM1-0208","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-11T05:00:00.000Z"],"dateStart":"2014-01-28T05:00:00.000Z","dateEnd":"2014-01-28T06:00:00.000Z"},{"summary":"CS2100 (EXAM)","description":"Computer Organisation","rrule":{"freq":"ONCE"},"dateStart":"2014-04-29T01:00:00.000Z","dateEnd":"2014-04-29T04:00:00.000Z"},{"summary":"CS1010 (SEC)","description":"Programming Methodology - ClassNo: 1","location":"i3-0345","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-25T02:00:00.000Z"],"dateStart":"2014-01-14T02:00:00.000Z","dateEnd":"2014-01-14T05:00:00.000Z"},{"summary":"CS1010 (TUT)","description":"Programming Methodology - ClassNo: 2","location":"COM1-B108","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-14T04:00:00.000Z"],"dateStart":"2014-01-31T04:00:00.000Z","dateEnd":"2014-01-31T06:00:00.000Z"},{"summary":"CS1010 (EXAM)","description":"Programming Methodology","rrule":{"freq":"ONCE"},"dateStart":"2014-04-30T09:00:00.000Z","dateEnd":"2014-04-30T12:00:00.000Z"}]');
-var nusmods_data;
+  'use strict';
 
-var now = moment();
-now = moment("2014-01-13").add(0, 'weeks'); // For testing purpose, set to 1st week of 2013-14Sem2.
+  var mock = JSON.parse('[{"summary":"SSA1201 (LECT)","description":"Singapore Society - ClassNo: 1","location":"LT11","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-24T08:00:00.000Z"],"dateStart":"2014-01-13T08:00:00.000Z","dateEnd":"2014-01-13T10:00:00.000Z"},{"summary":"SSA1201 (TUT)","description":"Singapore Society - ClassNo: E1","location":"AS1-0204","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-12T08:00:00.000Z"],"dateStart":"2014-01-29T08:00:00.000Z","dateEnd":"2014-01-29T10:00:00.000Z"},{"summary":"SSA1201 (EXAM)","description":"Singapore Society","rrule":{"freq":"ONCE"},"dateStart":"2014-04-30T01:00:00.000Z","dateEnd":"2014-04-30T04:00:00.000Z"},{"summary":"HY2242 (LECT)","description":"Singapore\'s Military History - ClassNo: 1","location":"AS4-0206","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-24T02:00:00.000Z"],"dateStart":"2014-01-13T02:00:00.000Z","dateEnd":"2014-01-13T04:00:00.000Z"},{"summary":"HY2242 (LECT)","description":"Singapore\'s Military History - ClassNo: 1","location":"AS4-0206","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-27T04:00:00.000Z"],"dateStart":"2014-01-16T04:00:00.000Z","dateEnd":"2014-01-16T06:00:00.000Z"},{"summary":"HY2242 (TUT)","description":"Singapore\'s Military History - ClassNo: W1","location":"AS1-0204","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-10T02:00:00.000Z"],"dateStart":"2014-01-27T02:00:00.000Z","dateEnd":"2014-01-27T04:00:00.000Z"},{"summary":"HY2242 (EXAM)","description":"Singapore\'s Military History","rrule":{"freq":"ONCE"},"dateStart":"2014-05-07T01:00:00.000Z","dateEnd":"2014-05-07T04:00:00.000Z"},{"summary":"GEK1008 (LECT)","description":"Southeast Asia: A Changing Region - ClassNo: 1","location":"UT-AUD1","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-28T06:00:00.000Z"],"dateStart":"2014-01-17T06:00:00.000Z","dateEnd":"2014-01-17T08:00:00.000Z"},{"summary":"GEK1008 (TUT)","description":"Southeast Asia: A Changing Region - ClassNo: D18","location":"ERC-SR11","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-14T08:00:00.000Z"],"dateStart":"2014-01-31T08:00:00.000Z","dateEnd":"2014-01-31T10:00:00.000Z"},{"summary":"GEK1008 (EXAM)","description":"Southeast Asia: A Changing Region","rrule":{"freq":"ONCE"},"dateStart":"2014-04-28T05:00:00.000Z","dateEnd":"2014-04-28T08:00:00.000Z"},{"summary":"CS2100 (LAB)","description":"Computer Organisation - ClassNo: 14","location":"COM1-0114","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-13T08:00:00.000Z"],"dateStart":"2014-01-30T08:00:00.000Z","dateEnd":"2014-01-30T09:00:00.000Z"},{"summary":"CS2100 (LECT)","description":"Computer Organisation - ClassNo: 1","location":"i3-Aud","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-26T04:00:00.000Z"],"dateStart":"2014-01-15T04:00:00.000Z","dateEnd":"2014-01-15T06:00:00.000Z"},{"summary":"CS2100 (LECT)","description":"Computer Organisation - ClassNo: 1","location":"i3-Aud","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-27T06:00:00.000Z"],"dateStart":"2014-01-16T06:00:00.000Z","dateEnd":"2014-01-16T07:00:00.000Z"},{"summary":"CS2100 (TUT)","description":"Computer Organisation - ClassNo: 13","location":"COM1-0208","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-11T05:00:00.000Z"],"dateStart":"2014-01-28T05:00:00.000Z","dateEnd":"2014-01-28T06:00:00.000Z"},{"summary":"CS2100 (EXAM)","description":"Computer Organisation","rrule":{"freq":"ONCE"},"dateStart":"2014-04-29T01:00:00.000Z","dateEnd":"2014-04-29T04:00:00.000Z"},{"summary":"CS1010 (SEC)","description":"Programming Methodology - ClassNo: 1","location":"i3-0345","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-02-25T02:00:00.000Z"],"dateStart":"2014-01-14T02:00:00.000Z","dateEnd":"2014-01-14T05:00:00.000Z"},{"summary":"CS1010 (TUT)","description":"Programming Methodology - ClassNo: 2","location":"COM1-B108","rrule":{"freq":"WEEKLY","count":14},"exclude":["2014-03-14T04:00:00.000Z"],"dateStart":"2014-01-31T04:00:00.000Z","dateEnd":"2014-01-31T06:00:00.000Z"},{"summary":"CS1010 (EXAM)","description":"Programming Methodology","rrule":{"freq":"ONCE"},"dateStart":"2014-04-30T09:00:00.000Z","dateEnd":"2014-04-30T12:00:00.000Z"}]');
+  var nusmods_data;
 
-var sunOfWeek = now.subtract(now.day(), 'days');
-var satOfWeek = moment(sunOfWeek).add(6, 'days');
+  var now = moment();
+  now = moment("2014-01-13").add(0, 'weeks'); // For testing purpose, set to 1st week of 2013-14Sem2.
 
-var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  var sunOfWeek = now.subtract(now.day(), 'days');
+  var satOfWeek = moment(sunOfWeek).add(6, 'days');
 
-// Adds an item to be shown on the timetable.
-var insertAndShowEvent = function(item) {
-  var dateStart = item.dateStart;
-  var dateEnd = item.dateEnd;
-  var day = moment(dateStart).day();
+  var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-  var color = '#c99'; // Temporary.
-  var CELL_WIDTH = 61; // Cell width.
-  var RIGHT_DIV_TRIM = 2; // Pixels to trim at the right of each item div.
+  // Adds an item to be shown on the timetable.
+  var insertAndShowEvent = function(item) {
+    var dateStart = item.dateStart;
+    var dateEnd = item.dateEnd;
+    var day = moment(dateStart).day();
 
-  var generateDiv = function(name, start, end) {
-    var div = $('<div>');
-    var duration = end.diff(start, 'h') * CELL_WIDTH;
-    div.width(duration - RIGHT_DIV_TRIM);
-    div.css('background-color', color);
-    div.addClass('item');
-    div.text(name);
-    return div;
+    var color = '#c99'; // Temporary.
+    var CELL_WIDTH = 61; // Cell width.
+    var RIGHT_DIV_TRIM = 2; // Pixels to trim at the right of each item div.
+
+    var generateDiv = function(name, start, end) {
+      var div = $('<div>');
+      var duration = end.diff(start, 'h') * CELL_WIDTH;
+      div.width(duration - RIGHT_DIV_TRIM);
+      div.css('background-color', color);
+      div.addClass('item');
+      div.text(name);
+      return div;
+    };
+
+    var div = generateDiv(item.summary, moment(dateStart), moment(dateEnd));
+    $('tr.' + days[day]).children('td.' + moment(dateStart).hour()).append(div);
   };
 
-  var div = generateDiv(item.summary, moment(dateStart), moment(dateEnd));
-  $('tr.' + days[day]).children('td.' + moment(dateStart).hour()).append(div);
-};
-
-var duringDisplayedWeek = function(item) {
-  var date;
-  var rruleCount = item.rrule.count;
-  // Check for excludes first.
-  if (item.exclude && item.exclude.length > 0) {
-    for (var i = 0; i < item.exclude.length; i++) {
-      date = moment(item.exclude[i]);
-      if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
-        return false;
+  var duringDisplayedWeek = function(item) {
+    var date;
+    var rruleCount = item.rrule.count;
+    // Check for excludes first.
+    if (item.exclude && item.exclude.length > 0) {
+      for (var i = 0; i < item.exclude.length; i++) {
+        date = moment(item.exclude[i]);
+        if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
+          return false;
+        }
       }
     }
-  }
 
-  // Then check for normal dates.
-  date = moment(item.dateStart);
-  if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
-    return true; // First date is during, return true.
-  }
-  else if (item.rrule.freq != 'ONCE') { // Repeating event...
-    rruleCount--; // Already tested first event date.
-    while (rruleCount > 0) {
-      if (item.rrule.freq === 'WEEKLY') {
-        date.add(1, 'week');
-      }
-
-      if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
-        return true; // Currently checked date is during, return true.
-      }
-
-      rruleCount--;
+    // Then check for normal dates.
+    date = moment(item.dateStart);
+    if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
+      return true; // First date is during, return true.
     }
-  }
-  return false;
-};
+    else if (item.rrule.freq != 'ONCE') { // Repeating event...
+      rruleCount--; // Already tested first event date.
+      while (rruleCount > 0) {
+        if (item.rrule.freq === 'WEEKLY') {
+          date.add(1, 'week');
+        }
 
-// Scrolls immediately to 7:00am.
-$('.tableWrapper').scrollLeft(428);
+        if (date.isAfter(sunOfWeek) && date.isBefore(satOfWeek)) {
+          return true; // Currently checked date is during, return true.
+        }
 
-// Clear event items.
-var clearItems = function() {
-  $('div.item').remove();
-};
-
-// Add NUSMods address.
-$('#add_nusmods').click(function(event) {
-  clearItems();
-  $.getJSON('/extract',
-    { addr: encodeURIComponent($('#nusmods_addr').val()) },
-    function(res) {
-      nusmods_data = res;
-      update();
-    }
-  );
-});
-
-// Update week number.
-//   Probably divide between NUS-style or default-style.
-var updateWeekNumber = function(){
-  $('#weekText').text('Week ' + now.week());
-};
-
-// Switch weeks.
-$('#left').click(function(event) {
-  now.add(-1, 'weeks');
-  update();
-});
-
-$('#right').click(function(event) {
-  now.add(1, 'weeks');
-  update();
-});
-
-var update = function() {
-  sunOfWeek = now.subtract(now.day(), 'days');
-  satOfWeek = moment(sunOfWeek).add(6, 'days');
-
-  clearItems();
-  updateWeekNumber();
-
-  if (nusmods_data) {
-    nusmods_data.forEach(function(item, index) {
-      if (duringDisplayedWeek(item)) {
-        insertAndShowEvent(item);
+        rruleCount--;
       }
+    }
+    return false;
+  };
+
+  // Scrolls immediately to 7:00am.
+  $('.tableWrapper').scrollLeft(428);
+
+  // Clear event items.
+  var clearItems = function() {
+    $('div.item').remove();
+  };
+
+  // Add NUSMods address.
+  $('#add_nusmods').click(function(event) {
+    clearItems();
+    $.getJSON('/extract',
+      { addr: encodeURIComponent($('#nusmods_addr').val()) },
+      function(res) {
+        nusmods_data = res;
+        update();
+      }
+    );
+  });
+
+  // Update week number.
+  //   Probably divide between NUS-style or default-style.
+  var updateDates = function(){
+    var date = moment(sunOfWeek);
+    $('#monthDisplay').text(date.format("MMMM YYYY"));
+
+    date.subtract(1, 'day'); // For Sunday.
+    $('.dayDate').each(function (index, day) {
+      $(day).text(date.add(1, 'day').format("DD MMM 'YY"));
     });
-  }
-};
-update();
+  };
+
+  // Switch weeks.
+  $('#left').click(function(event) {
+    now.add(-1, 'weeks');
+    update();
+  });
+
+  $('#right').click(function(event) {
+    now.add(1, 'weeks');
+    update();
+  });
+
+  var update = function() {
+    sunOfWeek = now.subtract(now.day(), 'days');
+    satOfWeek = moment(sunOfWeek).add(6, 'days');
+
+    clearItems();
+    updateDates();
+
+    if (nusmods_data) {
+      nusmods_data.forEach(function(item, index) {
+        if (duringDisplayedWeek(item)) {
+          insertAndShowEvent(item);
+        }
+      });
+    }
+  };
+  update();
 
 
 
-/*
-http://nusmods.com/2013-2014/sem2/v1/#HY2242=21&SSA1201=21&HY2242=8W1&CS1010=61&CS2100=813&CS2100=21&SSA1201=8E1&HY2242=21&CS2100=21&CS2100=114&CS1010=82&GEK1008=21&GEK1008=8D18
-*/
+  /*
+  http://nusmods.com/2013-2014/sem2/v1/#HY2242=21&SSA1201=21&HY2242=8W1&CS1010=61&CS2100=813&CS2100=21&SSA1201=8E1&HY2242=21&CS2100=21&CS2100=114&CS1010=82&GEK1008=21&GEK1008=8D18
+  */
+})();
